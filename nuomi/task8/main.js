f0072ea833c6019611589007018a42b40a07be74
hongzzz <wuchanghongzzz@gmail.com>
2017-09-27 18:21:37 +0800

    自定义右键菜单（修复滚动时候的bug）

--- nuomi/task8/main.js
+++ nuomi/task8/main.js
@@ -1,24 +1,27 @@
 let menu = document.getElementsByClassName('menu')[0];
 let content = document.getElementsByClassName('content');
 
+document.querySelector('body').oncontextmenu = (e) => {
+    return false;
+}
+
 for (let i = 0; i < content.length; i++) {
     let element = content[i];
     element.addEventListener('contextmenu', function (e) {
-        e.preventDefault();
         menu.style.display = 'block';
         let mwidth = menu.offsetWidth;
         let mheight = menu.offsetHeight;
         let clientheight = document.documentElement.clientHeight || document.body.clientHeight;
         let clientwidth = document.documentElement.clientWidth || document.body.clientWidth;
-        if (e.pageY + mheight < clientheight) {
-            menu.style.top = e.pageY + 'px';
+        if (e.clientY + mheight < clientheight) {
+            menu.style.top = e.clientY + window.pageYOffset + 'px';
         } else {
-            menu.style.top = e.pageY - mheight + 'px';
+            menu.style.top = e.clientY - mheight + window.pageYOffset + 'px';
         }
-        if (e.pageX + mwidth < clientwidth) {
-            menu.style.left = e.pageX + 'px';
+        if (e.clientX + mwidth < clientwidth) {
+            menu.style.left = e.clientX + window.pageXOffset + 'px';
         } else {
-            menu.style.left = e.pageX - mwidth + 'px';
+            menu.style.left = e.clientX + window.pageXOffset - mwidth + 'px';
         }
     });
 }
